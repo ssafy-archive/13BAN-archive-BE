6697b7a1eab194260da051afa81116a87e0c71da
luna156 <v010309@naver.com>
2025-06-07 11:36:43 +0900

    initial commit

---
+++ src/main/java/com/ssafy/ssafy_13ban_archive/Ssafy13banArchiveApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.ssafy_13ban_archive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ssafy13banArchiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ssafy13banArchiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ssafy/ssafy_13ban_archive/Ssafy13banArchiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.ssafy_13ban_archive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ssafy13banArchiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
