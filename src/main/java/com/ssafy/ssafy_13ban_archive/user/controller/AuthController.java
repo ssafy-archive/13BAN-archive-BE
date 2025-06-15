4fa1e4dd36c57bbc254b97692180fdc85f2efd50
H4ngL <a01031367140@gmail.com>
2025-06-15 15:06:58 +0900

    ARCHIVE-52 docs: user 관련 api에 swagger 문서 추가

--- src/main/java/com/ssafy/ssafy_13ban_archive/user/controller/AuthController.java
+++ src/main/java/com/ssafy/ssafy_13ban_archive/user/controller/AuthController.java
@@ -6,6 +6,9 @@ import com.ssafy.ssafy_13ban_archive.user.model.request.LoginRequestDTO;
 import com.ssafy.ssafy_13ban_archive.user.model.request.LogoutRequestDTO;
 import com.ssafy.ssafy_13ban_archive.user.model.response.LoginResponseDTO;
 import com.ssafy.ssafy_13ban_archive.user.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,11 +23,20 @@ public class AuthController {
 
     private final AuthService authService;
 
+    @Operation(summary = "로그인", description = "로그인 id와 password로 로그인을 진행합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "로그인 성공"),
+            @ApiResponse(responseCode = "403", description = "로그인 실패")
+    })
     @PostMapping("/login")
     public CommonResponse<LoginResponseDTO> login(@RequestBody LoginRequestDTO request) {
         return new CommonResponse<>(authService.login(request), HttpStatus.OK);
     }
 
+    @Operation(summary = "로그아웃", description = "로그아웃을 진행합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "로그아웃 성공"),
+    })
     @PostMapping("/logout")
     public CommonResponse<SuccessResponseDTO> logout(@RequestBody LogoutRequestDTO request) {
         return new CommonResponse<>(authService.logout(request), HttpStatus.OK);
--- src/main/java/com/ssafy/ssafy_13ban_archive/user/controller/UserController.java
+++ src/main/java/com/ssafy/ssafy_13ban_archive/user/controller/UserController.java
@@ -4,6 +4,9 @@ import com.ssafy.ssafy_13ban_archive.common.model.reponse.CommonResponse;
 import com.ssafy.ssafy_13ban_archive.user.model.request.SignInRequestDTO;
 import com.ssafy.ssafy_13ban_archive.user.model.response.SignInResponseDTO;
 import com.ssafy.ssafy_13ban_archive.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +21,10 @@ public class UserController {
 
     private final UserService userService;
 
+    @Operation(summary = "회원가입", description = "사용자 회원가입을 진행합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "회원가입 성공"),
+    })
     @PostMapping("/signIn")
     public CommonResponse<SignInResponseDTO> signIn(@RequestBody SignInRequestDTO request) {
         return new CommonResponse<>(userService.signIn(request), HttpStatus.OK);
