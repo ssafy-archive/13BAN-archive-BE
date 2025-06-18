6465575f4503d395e32d582a8708a3520b059dc1
luna156 <v010309@naver.com>
2025-06-18 23:01:10 +0900

    ARCHIVE-50 feat: 글 수정 및 삭제시 권한 체크 로직 추가

---
+++ src/main/java/com/ssafy/ssafy_13ban_archive/post/exception/PermisionDeniedException.java
@@ -0,0 +1,7 @@
+package com.ssafy.ssafy_13ban_archive.post.exception;
+
+public class PermisionDeniedException extends RuntimeException {
+    public PermisionDeniedException(String message) {
+        super(message);
+    }
+}
